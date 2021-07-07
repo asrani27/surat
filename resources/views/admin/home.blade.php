7c1e5954f72e912cd1b63e64eed933064da902e1
asrani <asrani.27@gmail.com>
2021-07-07 10:11:24 +0800

    FIX

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,9 +14,12 @@
                 <div class="user-block">
                   <img class="img-circle" src="/theme/dist/img/user1-128x128.jpg" alt="User Image">
                   <span class="username"><a href="#">{{Auth::user()->name}}</a></span>
-                  <span class="description">Jabatan</span>
+                  <span class="description">SELAMAT DATANG DI HALAMAN SUPERADMIN DEPARTEMEN AGAMA KALIMANTAN TENGAH</span>
                 </div>
-              </div>    
+            </div>
+            <div class="card-body text-center">
+              <img src="https://kalteng.kemenag.go.id/file/fotoberita/499734.jpg">
+            </div>    
         </div>
     </div>
 </div>
